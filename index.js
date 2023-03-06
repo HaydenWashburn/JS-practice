dea0f7cf8428bf46ba9fbd0b2f04cbca45962f7d
HaydenWashburn <hayden.washburn1@gmail.com>
2023-03-06 14:15:24 -0600

    created basic arguments and parameters

---
+++ index.js
@@ -0,0 +1,27 @@
+// PARAMETERS AND ARGUMENTS
+
+// 
+
+// let something="Hello World";
+
+// console.log(something)
+// console.log("Hello World")
+// String=Text inside quotes
+// Number=Any numbers not inside quotes
+
+// function cookies() {
+//     console.log("hello world")
+//     console.log(123456)
+// }
+
+// cookies()
+
+var args="hello bananas";
+var pizza="mama mia!!!"
+
+function bananas(params){
+    console.log(params)
+}
+
+bananas(args);
+bananas(pizza);
